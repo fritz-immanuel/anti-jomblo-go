69f249e8236075be57d22ad5432874fe28ee347e
Fritz Immanuel <fritz@atomic.id>
2024-06-10 17:59:51 +0700

    Add missing routes

--- src/app/mobile/routes.go
+++ src/app/mobile/routes.go
@@ -1,7 +1,9 @@
 package mobile
 
 import (
+	http_premiumpackage "anti-jomblo-go/src/app/mobile/premiumpackage"
 	http_user "anti-jomblo-go/src/app/mobile/user"
+	http_userpremium "anti-jomblo-go/src/app/mobile/userpremium"
 	http_userswipe "anti-jomblo-go/src/app/mobile/userswipe"
 
 	"anti-jomblo-go/library/data"
@@ -11,14 +13,18 @@ import (
 )
 
 var (
-	userHandler      http_user.UserHandler
-	userswipeHandler http_userswipe.UserSwipeHandler
+	premiumpackageHandler http_premiumpackage.PremiumPackageHandler
+	userHandler           http_user.UserHandler
+	userpremiumHandler    http_userpremium.UserPremiumHandler
+	userswipeHandler      http_userswipe.UserSwipeHandler
 )
 
 func RegisterRoutes(db *sqlx.DB, dataManager *data.Manager, router *gin.Engine, v *gin.RouterGroup) {
 	v1 := v.Group("")
 	{
+		premiumpackageHandler.RegisterAPI(db, dataManager, router, v1)
 		userHandler.RegisterAPI(db, dataManager, router, v1)
+		userpremiumHandler.RegisterAPI(db, dataManager, router, v1)
 		userswipeHandler.RegisterAPI(db, dataManager, router, v1)
 	}
 }
